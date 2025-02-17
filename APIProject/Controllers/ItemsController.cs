fbcf5c46626d2dd81cd55a7ad56e355132ae88c9
yunishuseynov99 <tatu99boy@gmail.com>
2025-02-17 14:06:27 +0400

    added itemsController with initial CRUD

---
+++ APIProject/Controllers/ItemsController.cs
@@ -0,0 +1,69 @@
+﻿using CatalogService.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<itemDto> _items = new()
+        {
+            new itemDto(Guid.NewGuid(), "Potion", "Restores a small amount of hp", 5, DateTimeOffset.UtcNow),
+            new itemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new itemDto(Guid.NewGuid(), "Brown sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+        };
+        [HttpGet]
+        public IEnumerable<itemDto> Get() 
+        {
+            return _items;
+        }
+
+        [HttpGet("{id}")]
+        public itemDto GetById(Guid id) 
+        {
+            var item = _items.Where(i => i.id == id).SingleOrDefault();
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<itemDto> Create([FromBody] CreateItemDto createItemDto) 
+        {
+            var item = new itemDto(Guid.NewGuid(), createItemDto.name, createItemDto.description, createItemDto.price, DateTimeOffset.UtcNow);
+            _items.Add(item);
+
+            return CreatedAtAction(nameof(GetById), new { id = item.id }, item);
+        }
+
+        [HttpPut("id")]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = _items.Where(i => i.id == id).SingleOrDefault();
+
+            var updatedItem = existingItem with
+            {
+                name = updateItemDto.name,
+                description = updateItemDto.description,
+                price = updateItemDto.price,
+            };
+
+            var index = _items.FindIndex(i => i.id == id);
+            _items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = _items.FindIndex(i => i.id == id);
+            _items.RemoveAt(index);
+
+            return NoContent();
+        }
+    }
+}
--- APIProject/DTOs.cs
+++ APIProject/DTOs.cs
@@ -2,9 +2,9 @@
 
 namespace CatalogService.DTOs
 {
-    public record itemDto(Guid id, string name, string desccription, decimal price, DateTimeOffset CreatedDate);
+    public record itemDto(Guid id, string name, string description, decimal price, DateTimeOffset CreatedDate);
 
     public record CreateItemDto(string name, string description, decimal price);
 
-    public record UpdateItem(string name, string Description, decimal price);
+    public record UpdateItemDto(string name, string description, decimal price);
 }
