941198eca2d6a4d1703556961c097e90913434b2
yunishuseynov99 <tatu99boy@gmail.com>
2025-02-17 13:20:52 +0400

    added DTOs

---
+++ APIProject/DTOs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CatalogService.DTOs
+{
+    public record itemDto(Guid id, string name, string desccription, decimal price, DateTimeOffset CreatedDate);
+
+    public record CreateItemDto(string name, string description, decimal price);
+
+    public record UpdateItem(string name, string Description, decimal price);
+}
