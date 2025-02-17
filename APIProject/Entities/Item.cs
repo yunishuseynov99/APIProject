6a11301c902ebeb16f3e65514432787730af0d6e
yunishuseynov99 <tatu99boy@gmail.com>
2025-02-17 16:35:36 +0400

    Added Items Repository

---
+++ APIProject/Entities/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CatalogService.Entities
+{
+    public class Item
+    {
+        public Guid  Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset CreatedDate { get; set;}
+    }
+}
